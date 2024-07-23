338f040b2c1ebfe5886355d3a6565872e6f87019
nhattruong-cmyk <155429462+nhattruong-cmyk@users.noreply.github.com>
2024-07-23 12:47:55 +0700

    verifycation-email

---
+++ resources/views/email-verified.blade.php
@@ -0,0 +1,8 @@
+
+<body class="mt-5">
+    <div class="container">
+        <h4>Email của bạn đã được xát minh!</h4>
+        <p>Tiếp tục trở về  <a href="{{ route('home') }}">Trang Chủ</a>.</p>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,6 @@
 <?php
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -64,14 +66,40 @@ Route::post('/updateUser', [AdminController::class, 'updateUser'])->name('update
 Route::get('/admin/search', [AdminController::class, 'search'])->name('admin.search');
 
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile/update', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
+// verifycation email
+
+
+Route::get('/dashboard', function () {
+    return view('email-verified');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+
+    return redirect()->route('email.verified');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::get('/email-verified', function () {
+    return view('email-verified');
+})->name('email.verified');
+
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+ 
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
 require __DIR__.'/auth.php';
