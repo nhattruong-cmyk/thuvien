1a89dd5b83bf790905615211aa4004dca292973c
tiencmpc05891 <145202677+tiencmpc05891@users.noreply.github.com>
2024-08-03 00:05:25 +0700

    update thành công

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -22,4 +22,8 @@ class Product extends Model
         return $this->belongsTo(Category::class, 'category_id');
 
     }
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
